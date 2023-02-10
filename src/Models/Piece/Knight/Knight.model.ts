1ddb00c1c32e6497a1f2bf86add5bd977b76d6b6
António Rocha <afgrocha98@gmail.com>
2023-02-10 18:56:58 +0000

    Knight EX Move

--- src/Models/Piece/Knight/Knight.model.ts
+++ src/Models/Piece/Knight/Knight.model.ts
@@ -2,15 +2,26 @@ import * as ex from 'excalibur';
 import { smallDistanceMixin } from '../../../Mixins/SmallMovementPiece.mixin';
 import { Chess } from '../../../Scenes/chess';
 import { piecesInPlay } from '../../../State/Grid.state';
+import { AvailableMove } from '../../AvailableMove/AvailableMove.model';
 import { TilePosition } from '../../Board/Board.model';
 import { Piece, PiecePosition } from '../Piece.model';
 
 const smallDistancePiece = smallDistanceMixin(Piece)
 
 export class Knight extends smallDistancePiece {
-    
+    directionModifier = [
+        {x: -1, y: -2}, // UpLeft
+        {x: -2, y: -1}, // LeftUp
+        {x: -1, y: 2}, // DownLeft
+        {x: -2, y: 1}, // LeftDown
+        {x: 1, y: -2}, // UpRight
+        {x: 2, y: -1}, // RightUp
+        {x: 2, y: 1}, // RightDown
+        {x: 1, y: 2} // DownRight
+    ]
     constructor(asset: ex.ImageSource, tilePosition: PiecePosition, grid: TilePosition[][], pieceColor: string, chess: Chess ) { 
         super(asset,tilePosition,grid, pieceColor, `${pieceColor}Knigh${tilePosition.col}`, chess);
+        this.exAmount = 200
     }
 
     onInitialize() {
@@ -18,19 +29,30 @@ export class Knight extends smallDistancePiece {
     }
 
     select() {
-        const directionModifier = [
-            {x: -1, y: -2}, // UpLeft
-            {x: -2, y: -1}, // LeftUp
-            {x: -1, y: 2}, // DownLeft
-            {x: -2, y: 1}, // LeftDown
-            {x: 1, y: -2}, // UpRight
-            {x: 2, y: -1}, // RightUp
-            {x: 2, y: 1}, // RightDown
-            {x: 1, y: 2} // DownRight
-        ]
-
-        this.smallDistanceMove(directionModifier, piecesInPlay)
+        if(!this.chess?.exMeter.isOn)
+            this.smallDistanceMove(this.directionModifier, piecesInPlay)
 
         super.select()
+    }
+
+    DrawExMove() {
+        for (var col in piecesInPlay) {
+            for(var row in piecesInPlay[col]) {
+                if(!piecesInPlay[col][row]) {
+                    const x = parseInt(col)
+                    const y = parseInt(row)
+                    const vectorX = (x - this.currentPosition.col) * 100
+                    const vectorY = (y - this.currentPosition.row) * 100
+                    const vector = new ex.Vector(vectorX, vectorY)
+                    const availableMove = new AvailableMove(vector, this.availableTileColor)
+                    availableMove.on('pointerdown', () => {
+                        this.move(x, y)
+                        console.log(piecesInPlay)
+                    })
+                    this.addChild(availableMove)
+                    this.availableTiles.push(availableMove)
+                } 
+            } 
+        }
     }
 }
