c7c437eb57302026f87ff004fcf58fcc4d040be3
António Rocha <afgrocha98@gmail.com>
2023-06-15 13:17:38 +0100

    Adding dist

--- src/State/Grid.state.ts
+++
@@ -1,8 +0,0 @@
-const piecesInPlay: any[][] = []
-for (let i = 0; i < 8; i++) {
-    piecesInPlay.push([])
-    for(let j = 0; j < 8; j++) {
-        piecesInPlay[i].push(null) 
-    }
-}
-export {piecesInPlay}
--- src/State/History.state.ts
+++
@@ -1,5 +0,0 @@
-import $ from "jquery";
-
-const $gameHistory = $('#history-container'); // cache the reference to the HTML list element
-
-export {$gameHistory}
